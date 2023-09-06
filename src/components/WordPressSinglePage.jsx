3786d3192f567ab1950f7186ef6fd05e11c13b01
etran <etran@bluecatnetworks.com>
2023-09-06 13:21:10 +0700

    Strapi

--- src/components/WordPressSinglePage.jsx
+++ src/components/WordPressSinglePage.jsx
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
+const TOKEN =
+    'b6d5cd12b8166f9e185ac0336b7da30f10299db766ba24869a00f2012d4367a0830d9682f4e7c71ca098bdac2b496376b0e57b8a7074ad29abf448a9e11d38ec74b6d40cf1d02ad2d0c951858d6e82b7a531b8fc7056596e98e12a346e9c1466e9e906d030eea05dae4dbf9b1f2da7f4aa442e97f4436d74957ddcaac193826d';
+
 const WordPressSinglePage = () => {
     const [pageContent, setPageContent] = useState(null);
 
+    // useEffect(() => {
+    //     fetch('http://172.104.167.168:32768/wp-json/wp/v2/pages/18')
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setPageContent(data.content.rendered);
+    //         })
+    //         .catch((error) => console.log('Error fetching page:', error));
+    // }, []);
+
     useEffect(() => {
-        fetch('http://172.104.167.168:32768/wp-json/wp/v2/pages/18')
+        fetch('http://localhost:1337/api/Articles?populate=*', {
+            headers: {
+                'Authorization': `Bearer ${TOKEN}`,
+            },
+        })
             .then((response) => response.json())
-            .then((data) => {
-                setPageContent(data.content.rendered);
-            })
-            .catch((error) => console.log('Error fetching page:', error));
+            .then((data) => console.log(data))
+            .catch((error) => console.log('Error:', error));
     }, []);
 
     return (
---
+++ src/pages/About06Page/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About06Page(props) {
+    return <div></div>;
+}
+
+export default About06Page;
